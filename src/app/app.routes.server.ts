9b0fa749014f8a1b09e1bfc7a8e4e6de48603b85
maria <maria.leiliane.lobo@gmail.com>
2026-02-13 07:45:19 -0300

    deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,11 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-
+  // 1. Tratamos a rota de edição que está causando o erro no deploy
   {
     path: 'habits/editar/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender,
+    // Usamos async e a tipagem correta para satisfazer o TypeScript
+    getPrerenderParams: async (): Promise<Record<string, string>[]> => {
+      return [];
+    }
   },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender
