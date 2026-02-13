4e6473acf0d6382758362b111b8a663441e4f569
maria <maria.leiliane.lobo@gmail.com>
2026-02-13 07:27:41 -0300

    deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  {
+    path: 'habits/editar/:id',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
